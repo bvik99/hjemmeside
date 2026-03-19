5dff1e38d8c52a381c8f3871b3b6d5b22c19eab8
Bernhard Vik <bernhard.vik@hotmail.com>
2026-03-19 17:11:59 +0100

    bvik99

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-center">
+    <div className="text-center mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
       <br />
       <h1 className="text-6xl">Programmering</h1>
       <br />
       <br />
-      <p>Ulike temaer som er aktuelle å lære om:</p>
+      <p className="text-3xl">Ulike temaer som er aktuelle å lære om:</p>
+      <br />
       <ul className="text-3xl">
         <li>Python</li>
         <li>Java</li>
