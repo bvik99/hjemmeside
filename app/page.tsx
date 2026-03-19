4b69046a2391f66ea4d2a8524efe77b9c229a611
Bernhard Vik <bernhard.vik@hotmail.com>
2026-03-19 17:49:42 +0100

    bvik99

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-center gap-20 max-w-sm mx-auto bg-blue-500/75">
-      <h1 className="text-6xl font-medium text-#D3D3D3">Programmering</h1>
+    <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+      <h1 className="text-6xl font-medium">Programmering</h1>
       <div className="text-3xl">
         <p>
           Ulike temaer som er aktuelle å lære om:
