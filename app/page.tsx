3876fe1a93ddcda52056afa8b52f35101d37bdb2
Bernhard Vik <bernhard.vik@hotmail.com>
2026-03-19 16:57:30 +0100

    bvik99

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-2xl">Programmering</h1>
+      <h1 className="text-6xl">\nProgrammering\n</h1>
       <p>Ulike temaer som er aktuelle å lære om:</p>
-      <ul>
+      <ul className="text-3xl">
         <li>Python</li>
         <li>Java</li>
         <li>Javascript</li>
